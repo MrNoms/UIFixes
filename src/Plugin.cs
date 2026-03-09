f17efb1702b1dea2ff8c65219a7e0ab0701c86a2
mrnoms <tiddle96@gmail.com>
2026-03-09 01:39:23 -0400

    Remove unused statements

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Reflection;
 using BepInEx;
 using BepInEx.Bootstrap;
 using BepInEx.Logging;
 using Comfort.Common;
 using EFT;
-using SPT;
-using SPT.Reflection.Utils;
 using TMPro;
 using UnityEngine.EventSystems;
 
@@ -117,18 +114,6 @@ public class Plugin : BaseUnityPlugin
         return EventSystem.current?.currentSelectedGameObject != null &&
             EventSystem.current.currentSelectedGameObject.activeInHierarchy &&
             EventSystem.current.currentSelectedGameObject.GetComponent<TMP_InputField>() != null;
-    }
-
-    private void CheckForTypeChanges()
-    {
-        foreach (var type in PatchConstants.EftTypes)
-        {
-            var attr = type.GetCustomAttribute<SPTRenamedClassAttribute>();
-            if (attr != null && attr.HasChangesFromPreviousVersion)
-            {
-                Logger.LogInfo($"Type: {type.Name} has had changes!");
-            }
-        }
     }
 
     private static bool? IsFikaPresent;
